8053ef78d81c1dbb8b6997aef708bd976d07b387
Song Khoi <songkhoi123@gmail.com>
2025-04-01 12:48:23 +0700

    fix ip

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,17 @@ import { type NextRequest, NextResponse } from "next/server";
 export default function middleware(req: NextRequest) {
 	const { pathname } = req.nextUrl;
 
+	// Lấy địa chỉ IP của client
+	// Nếu có x-forwarded-for, lấy IP đầu tiên (IP thật của client)
 	const forwardedFor = req.headers.get("x-forwarded-for") as string;
+	const connectionRemoteIp = req.headers.get("x-real-ip") as string;
 	const clientIp = forwardedFor
 		? forwardedFor.split(",")[0].trim()
-		: req.ip || "";
+		: connectionRemoteIp || "";
 
+	// Chỉ cho phép /warranty từ public
 	if (!pathname.startsWith("/warranty")) {
+		// Kiểm tra xem IP có phải là local không
 		const isLocalNetwork =
 			clientIp === "127.0.0.1" ||
 			clientIp.startsWith("10.100.0.") ||
@@ -20,6 +25,7 @@ export default function middleware(req: NextRequest) {
 		}
 	}
 
+	// Tiếp tục request nếu là path /warranty hoặc từ mạng local
 	return NextResponse.next();
 }
 
