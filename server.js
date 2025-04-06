c23a0d7500f62457accdce6be462303829a79cff
Song Khoi <songkhoi123@gmail.com>
2025-04-06 11:26:31 +0700

    add https

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+	key: fs.readFileSync("/etc/letsencrypt/live/yourdomain.com/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/yourdomain.com/fullchain.pem"),
+};
+
+app.prepare().then(() => {
+	createServer(httpsOptions, (req, res) => {
+		const parsedUrl = parse(req.url || "", true);
+		handle(req, res, parsedUrl);
+	}).listen(443, (err) => {
+		if (err) throw err;
+		console.log("> Ready on https://yourdomain.com");
+	});
+});
