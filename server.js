6e0e7c5b8874222f68269ef7f2a5c5ff32ca947c
Song Khoi <songkhoi123@gmail.com>
2025-09-30 15:55:01 +0700

    fix

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.prepare().then(() => {
 	createServer(httpsOptions, (req, res) => {
 		const parsedUrl = parse(req.url || "", true);
 		handle(req, res, parsedUrl);
-	}).listen(443, (err) => {
+	}).listen(3000, (err) => {
 		if (err) throw err;
-		console.log("> Ready on https://dl.lug.center");
+		console.log("> Ready on https://dl.lug.center:3000");
 	});
 });
